4ce43e18735335bdcb02699b5d9269ab3ccabc1b
PG <frankpg267@gmail.com>
2020-08-23 11:36:05 +0800

    修改部屬檔案

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/pigeonchen/KartRider-Shop.git master:gh-pages
+git push -f https://github.com/boboyuki/K-Shop.git master:gh-pages
 
 cd -
